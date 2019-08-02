03fbe2553ffa4477ac18a6093c6e3ed00537e29e
fernandomazola <fernando_mazola@hotmail.com>
2019-08-02 12:05:23 -0300

    projeto finalizado

--- aula16/exercicio/script.js
+++ aula16/exercicio/script.js
@@ -1,9 +1,7 @@
-var res = document.querySelector('#analise');
-var numero = document.querySelector('input#numero');
-var btnadicionar = document.querySelector('#btn-adicionar');
-var lista = document.querySelector('select#selectbox');
-var btnfinalizar = document.querySelector('#btn-finalizar');
-var valoresLista = [];
+let res = document.querySelector('#info');
+let lista = document.querySelector('#selectbox');
+let numero = document.querySelector('#numero');
+let valoresLista = [];
 
 
 
@@ -17,20 +15,49 @@ function isNumero(numeroCampo) {
     }
 }
 
-function isLista(numeroCampo, valoresLista) {
-    if (valoresLista.indexOf(numeroCampo) == -1) {
+function inLista(numeroCampo, valoresLista) {
+    if (valoresLista.indexOf(Number(numeroCampo)) == -1) {
         return true;
     } else {
         return false;
     }
 }
 
-
 function adicionar() {
     if (isNumero(numero.value) && inLista(numero.value, valoresLista)) {
-        valoresLista.push(numero.value);
-        createElement
+        valoresLista.push(Number(numero.value));
+        var option = document.createElement('option');
+        option.text = `Valor ${numero.value} foi adicionado`;
+        lista.appendChild(option);
+        res.innerHTML = '';
     } else {
         alert('Valor inválido ou já encontrado na lista');
+    }
+    numero.value = '';
+    numero.focus();
+}
+
+function finalizar(){
+    if(valoresLista.length == 0){
+        alert('Adicione um valor na lista');
+    }else{
+        let total = valoresLista.length;
+        let maiornum = valoresLista[0];
+        let menornum = valoresLista[0];
+        let soma = 0;
+        res.innerHTML += `<p>Ao todo temos ${total} cadastrados</p>`;
+        for (let pos in valoresLista){
+            if(valoresLista[pos] > maiornum){
+                maiornum = valoresLista[pos];
+            }
+            if(valoresLista[pos] < menornum){
+                menornum = valoresLista[pos];
+            }
+            soma += valoresLista[pos];
+        }
+        res.innerHTML += `<p>O maior número informado é ${maiornum}</p>`;
+        res.innerHTML += `<p>O menor número informado é ${menornum}</p>`;
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`;
+        res.innerHTML += `<p>A média dos valores digitados é ${soma / valoresLista.length}</p>`
     }
 }
